ffc8a60e4bd11d0f9fef701a82784f032a145a30
Bertrand Jouin <bertrandjouin@gmail.com>
2020-02-13 16:55:20 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+puts "Creating categories..."
+
+gin = Cocktail.create!(name: "Gin To")
+sunrise = Cocktail.create!(name: "Tequila Sunrise")
+caipirina = Cocktail.create!(name: "caipirina")
+tomcollins = Cocktail.create!(name: "tom collins")
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+menthole = Ingredient.create!(name: "menthol")
+salt = Ingredient.create!(name: "salt")
+tequila = Ingredient.create!(name: "Tequila")
+sucredecanne = Ingredient.create!(name: "sucre de canne")
+gin = Ingredient.create!(name: "gin")
+
+Dose.create!(description: "12cl", cocktail_id: gin.id, ingredient_id: lemon.id)
+Dose.create!(description: "9cl", cocktail_id: sunrise.id, ingredient_id: tequila.id)
+Dose.create!(description: "9cl", cocktail_id: tomcollins.id, ingredient_id: gin.id)
+puts "Finished!"
